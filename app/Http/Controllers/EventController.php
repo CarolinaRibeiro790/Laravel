38c6e4efa3cfb93c6eae67973d1c442bda649a7d
CarolinaRibeiro790 <carolribeirog20@gmail.com>
2025-01-03 22:08:16 -0300

    Cancelar evento

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -75,9 +75,22 @@ class EventController extends Controller
     {
         $event = Event::findOrFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if ($user) {
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach($userEvents as $userEvent){
+                if($userEvent['id'] == $id ){
+                    $hasUserJoined = true;   
+                }
+            }
+        }
+
         $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner, 'hasUserJoined' => $hasUserJoined]);
     }
 
     public function dashboard()
@@ -89,7 +102,7 @@ class EventController extends Controller
 
         $eventsAsParticipant = $user->eventsAsParticipant;
 
-        return view('events.dashboard', ['events' => $events, 'eventsAsParticipant' => $eventsAsParticipant ]);
+        return view('events.dashboard', ['events' => $events, 'eventsAsParticipant' => $eventsAsParticipant]);
     }
 
     public function destroy($id)
@@ -107,7 +120,7 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
-        if($user->id != $event->user_id){
+        if ($user->id != $event->user_id) {
             return redirect('/dashboard');
         }
 
@@ -140,7 +153,7 @@ class EventController extends Controller
         Event::findOrFail($request->id)->update($data);
 
         return redirect('/dashboard')->with('msg', 'Evento editado com sucesso!');
-    }   
+    }
 
     public function joinEvent($id)
     {
@@ -152,5 +165,14 @@ class EventController extends Controller
         return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento ' . $event->title);
     }
 
+    public function leaveEvent($id)
+    {
+        $user = auth()->user();
 
+        $user->eventsAsParticipant()->detach($id); //remove a ligação
+
+        $event = Event::findOrFail($id);
+
+        return redirect('/dashboard')->with('msg', 'Você saiu com sucesso do evento ' . $event->title);
+    }
 }
